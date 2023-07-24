fe8cfe4bc7e959ceeec8eaf05664ecd3c5adda3c
Naveen Prakash <naveenprakashjk24@gmail.com>
2023-07-24 21:04:44 +0530

    curd oprations

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, sessionLocal
 from sqlalchemy.orm import Session
@@ -15,7 +15,7 @@ def get_db():
         db.close()
     
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def createBlog(request:schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Bolg(title=request.title, body=request.body)
     db.add(new_blog)
@@ -29,6 +29,18 @@ def allBlogs(db: Session = Depends(get_db)):
     return blogs
 
 @app.get('/blog/{id}')
-def blog(id:int, db: Session = Depends(get_db)):
+def blog(id:int,responce : Response, db: Session = Depends(get_db)):
     blogs = db.query(models.Bolg).filter(models.Bolg.id==id).first()
+    # if not blogs:
+    #     responce.status_code = status.HTTP_404_NOT_FOUND
+    #     return {'details':f"Blog with the id {id} is not available"}
+    if not blogs:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
+        
     return blogs
+
+@app.delete('/blogdlt/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delblog(id:int,responce : Response, db: Session = Depends(get_db)):
+    db.query(models.Bolg).filter(models.Bolg.id==id).delete(synchronize_session=False)
+    db.commit()
+    return 'Blog deleted successfully'
