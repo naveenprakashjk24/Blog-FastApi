2b3d1d2bea57c7b57a18d8cec9f2e4b2419d3ac3
Naveen Prakash <naveenprakashjk24@gmail.com>
2023-11-26 10:59:57 +0530

    working on alembic migration

--- blog/database.py
+++ blog/database.py
@@ -2,14 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
 
 #Postgres connection
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:jkn;@localhost/TodoApplication"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:jkn;@localhost/TodoApplication"
 
 #Db connection
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 #create session
 sessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
